68aabbfe78a812402222223981aa5c4153215f62
Jesse Sandvik <jsandvik89@gmail.com>
2022-06-14 18:17:01 -0400

    initialized react-router-dom, router and routes setup to accept routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+import Footer from "./components/organisms/footer/Footer";
+import Header from "./components/organisms/header/Header";
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header></Header>
+      <Routes></Routes>
+      <Footer>&copy; jessesandvik {new Date().getFullYear()}</Footer>
+    </div>
+  );
 };
 
 export default App;
