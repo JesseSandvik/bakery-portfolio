467bcc623d84d657b9425799166c7f701a410bea
Jesse Sandvik <jsandvik89@gmail.com>
2022-07-16 16:09:38 -0400

    created image component, added basic layout and stying for homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,19 @@
+import Heading from "../../components/atoms/heading/Heading";
+import Image from "../../components/atoms/image/Image";
+import HomepageBanner from "../../images/banner-homepage.png";
+
 function Home() {
-  return <main />;
+  return (
+    <main>
+      <Heading level="2">experience the magic...</Heading>
+      <div id="image-carousel">
+        <Image
+          src={HomepageBanner}
+          alt="A collection of Stacy's cake sample images."
+        />
+      </div>
+    </main>
+  );
 }
 
 export default Home;
