c598b24cb1125c1a524504740ff68090584714c2
Jesse Sandvik <jsandvik89@gmail.com>
2022-07-20 22:57:33 -0400

    added convience features and routing to homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,9 @@
 import Button from "../../components/atoms/button/Button";
 import Heading from "../../components/atoms/heading/Heading";
 import Image from "../../components/atoms/image/Image";
+import NavigationLink from "../../components/atoms/navLink/NavLink";
 import Paragraph from "../../components/atoms/paragraph/Paragraph";
+import Small from "../../components/atoms/small/Small";
 import WhiteCake from "../../images/cake-white.jpg";
 
 function Home() {
@@ -16,20 +18,27 @@ function Home() {
           custom handmade <span>cakes</span>, <span>cupcakes</span>, &{" "}
           <span>treats</span>
         </Heading>
-        <Heading level="3">prepared fresh from our kitchen</Heading>
+        <Heading level="3">prepared fresh from our kitchen,</Heading>
         <Heading level="2">delivered straight to your doorstep!</Heading>
         <span id="btn-group">
           <Button className="btn-primary">view our menu</Button>
           <Button className="btn-secondary">view custom order options</Button>
         </span>
+        <Small>
+          Ready to order? View our{" "}
+          <NavigationLink to="/contact">booking policy</NavigationLink>.
+        </Small>
       </div>
       <div className="container">
         <Heading level="3">now serving nassau & suffolk county!</Heading>
         <span id="social-icons">
-          <Button>
+          <Button title="Email">
+            <i className="fa-solid fa-square-envelope" />
+          </Button>
+          <Button title="Facebook">
             <i className="fa-brands fa-facebook-square" />
           </Button>
-          <Button>
+          <Button title="Instagram">
             <i className="fa-brands fa-instagram-square" />
           </Button>
         </span>
